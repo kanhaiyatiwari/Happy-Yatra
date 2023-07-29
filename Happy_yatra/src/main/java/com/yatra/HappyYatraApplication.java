728deba675536566620ad04e6695b8160f6e6ee8
kanhaiyatiwari <tiwarikanhaiya149@gmail.com>
2023-07-29 16:29:16 +0530

    add

---
+++ Happy_yatra/src/main/java/com/yatra/HappyYatraApplication.java
@@ -0,0 +1,13 @@
+package com.yatra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyYatraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyYatraApplication.class, args);
+	}
+
+}
